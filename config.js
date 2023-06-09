31531689add1c3b810411b008ce9dc4455a5c269
HoangLamDZ <78650276+HoangLamDZ@users.noreply.github.com>
2023-06-09 21:43:38 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Khánh Ly <3 😘",
+    introTitle: 'HI Khánh Ly <3😘',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: '🥰Ngồi chờ duyên phận ý trời',
+    desc: 'Phải chăng Chi bằng chủ động ngỏ lời thương nhau! ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Học sâu, hiểu rộng, biết nhiều. Chi bằng gặp mặt đánh liều yêu cậu! <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'chúc cậu mùng 8/3 vui vẻ ngày càng xinh đẹp trong mắt tớ nhaaaaa, iu cậu nhìu ạ <33333.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: https://www.facebook.com/dii.hanthien.90857' 
+}
